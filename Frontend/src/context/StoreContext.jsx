a1a3765c941728461678748fc7b2b6f2d6aa7e4a
Prashant88123 <sprashant1103@gmail.com>
2026-09-13 10:43:31 +0530

    configure production api url

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -9,8 +9,8 @@ const StoreContextProivder= (props)=>{
     const[cartItems,setCartItems]=useState({});
     const [token,setToken]=useState("");
     const [food_list,setFoodList]=useState([]);
-    const url='http://localhost:4000';
-
+    const url=import.meta.env.VITE_API_URL;
+    
     const addToCart=async (itemId)=>{
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev,[itemId]:1}))
