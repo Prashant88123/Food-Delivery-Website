7de1e8dc522f29b5ef15274681ebf60e68c3af49
Prashant88123 <sprashant1103@gmail.com>
2026-09-13 10:57:23 +0530

    fix MyOrders import

--- Frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ Frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Exploremenu.css'
+import './ExploreMenu.css'
 import { menu_list } from '../../assets/assets'
 
 const Exploremenu = ({category,setCategory}) => {
